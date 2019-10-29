302dd9deb690bfde495a0ee6e8ea15afa05b1cd7
Trisnapuspita <trisna0401@gmail.com>
2019-10-29 19:24:07 +0700

    now

--- app/Exports/ReportExport.php
+++ app/Exports/ReportExport.php
@@ -4,27 +4,40 @@ namespace App\Exports;
 
 use App\Models\ArticleEdition;
 use Illuminate\Contracts\View\View;
-use Maatwebsite\Excel\Concerns\FromCollection;
-use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 
-class ReportExport implements FromCollection, WithHeadings, ShouldAutoSize
+class ReportExport implements FromView, WithHeadings, ShouldAutoSize
 {
-
-    public function collection()
+    public function __construct(string $article_title, string $writer, string $desc, string $keyword)
     {
-        return ArticleEdition::select('article_title', 'writer', 'source', 'desc', 'keyword')->get();
+        $this->article_title = $article_title;
+        $this->writer = $writer;
+        $this->desc = $desc;
+        $this->keyword = $keyword;
     }
 
-    public function headings(): array
+    public function view(): View
     {
-        return [
-            'Judul',
-            'Pengarang',
-            'Sumber',
-            'Deskripsi Singkat',
-            'Kata Kunci',
-        ];
+        return view('reports.indeks', [
+            'data' => ArticleEdition::where('article_title','like', '%'.$this->article_title.'%')->where('writer', 'like', '%'.$this->writer.'%')->where('desc', 'like', '%'.$this->desc.'%')->where('keyword', 'like', '%'.$this->keyword.'%')->get()
+        ]);
     }
+
+    // public function collection()
+    // {
+    //     return ArticleEdition::select('article_title', 'writer', 'source', 'desc', 'keyword')->get();
+    // }
+
+    // public function headings(): array
+    // {
+    //     return [
+    //         'Judul',
+    //         'Pengarang',
+    //         'Sumber',
+    //         'Deskripsi Singkat',
+    //         'Kata Kunci',
+    //     ];
+    // }
 }
