0ff581808cd6bc013a97e1aac522f09e20e5297c
Trisnapuspita <37279294+Trisnapuspita@users.noreply.github.com>
2019-12-24 23:49:28 +0700

    Update and rename 2019_10_08_041632_update_edition_table.php to 2019_10_08_041632_update_edition_tablee.php

--- database/migrations/2019_10_08_041632_update_edition_tablee.php
+++ database/migrations/2019_10_08_041632_update_edition_tablee.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateEditionTable extends Migration
+class UpdateEditionTablee extends Migration
 {
     /**
      * Run the migrations.
